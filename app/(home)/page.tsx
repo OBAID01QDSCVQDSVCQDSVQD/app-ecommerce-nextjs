b35e46d5a7f197361146ce8b9676725abdca593e
OBAID01QDSCVQDSVCQDSVQD <OBAIDESSAFI@gmail.com>
2025-05-23 00:35:44 +0200

    page-details-product

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -5,6 +5,7 @@ import data from '@/lib/data'
 import { getAllCategories, getProductsForCard, getProductsByTag } from '@/lib/actions/product.actions' 
 import { toSlug } from '@/lib/utils'
 import ProductSlider from '@/components/shared/product/product-slider'
+import BrowsingHistoryList from '@/components/shared/browsing-history-list'
 
 export default async function HomePage() {
   const categories = (await getAllCategories()).slice(0, 4)
@@ -81,6 +82,9 @@ export default async function HomePage() {
           </CardContent>
         </Card>
       </div>
+      <div className='p-4 bg-background'>
+  <BrowsingHistoryList />
+</div>
     </>
   )
 }
